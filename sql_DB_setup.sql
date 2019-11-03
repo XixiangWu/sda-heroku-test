f861847896533e3b599ca27bfd1f738ef2876ac1
XixiangWu <xixiangwu0108@gmail.com>
2019-11-04 02:08:31 +1100

    dbConnection

--- sql_DB_setup.sql
+++ sql_DB_setup.sql
@@ -1,12 +1,12 @@
--- DROP TABLE books;
---
--- CREATE TABLE books(
---     isbn INT,
---     title VARCHAR(50),
---     PRIMARY KEY (isbn)
--- );
---
--- INSERT INTO books VALUES (1001, 'first book');
---
+DROP TABLE books;
+
+CREATE TABLE books(
+    isbn INT,
+    title VARCHAR(50),
+    PRIMARY KEY (isbn)
+);
+
+INSERT INTO books VALUES (1001, 'first book');
+
 
 SELECT * FROM books;
--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,5 +1,7 @@
 package database;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,7 +10,6 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static String DB_CONNECTION = "jdbc:postgresql://localhost:5432/postgres";
-
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "xindefengbao12";
 
@@ -34,12 +35,19 @@ public class DBConnection {
             // Register JDBC driver
             DriverManager.registerDriver(new org.postgresql.Driver());
 
-            dbConnection = DriverManager.getConnection(DB_CONNECTION,DB_USER,DB_PASSWORD);
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+//            dbConnection = DriverManager.getConnection(DB_CONNECTION,DB_USER,DB_PASSWORD);
+            dbConnection = DriverManager.getConnection(dbUrl, username, password);
 
             dbConnection.setAutoCommit(false);
 
             return dbConnection;
-        } catch (SQLException e) {
+        } catch (SQLException | URISyntaxException e) {
 
             System.out.println(e.getMessage());
 
